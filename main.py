0251ed71d1ab62c55d53f5b39215e528bf3abf9c
SimonHalvdansson <simonhalvdansson@gmail.com>
2017-05-16 13:27:35 +0200

    Cleanup

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def compress_no_matrices(A):
     """
     A, M, N = preprocess_matrix(A)
     
+    
+    """
     for a in range(M):
         for b in range(N//2):
             B[a, b] = (A[a, 2 * b]+A[a,2*b+1])/2
@@ -68,24 +70,22 @@ def compress_no_matrices(A):
             C[b,a] = (B[b*2,a] + B[b*2+1,a])/2
         for b in range(M//2):   
             C[b+M//2,a] = (B[b*2,a] - B[b*2+1,a])/2
-    
+    """
     
     
     
 
-def inverse_transformation(B1, B2 = None, B3 = None, B4 = None):
+def inverse_transformation_piecewise(B1, B2, B3, B4):
+    Q1 = np.column_stack([B1, B2])
+    Q2 = np.column_stack([B3, B4])
+    return inverse_transformation(np.vstack([Q1, Q2]))
+
+def inverse_transformation(B):
     """
     Takes a full compressed image B or B in parts B1, B2, B3, B4
     with diff information and returns the original matrix by
     doing the inverse transformation.
     """
-    
-    if not B4 == None:
-        np.column_stack(B1, B2)
-        np.column_stack(B3, B4)
-        np.vstack(B1, B3)
-        
-    B = B1
     
     M = len(B[:, 0])
     N = len(B[0, :])
@@ -97,40 +97,38 @@ def inverse_transformation(B1, B2 = None, B3 = None, B4 = None):
     
     return A
 
-
-
-A = sm.imread('kvinna.jpg', True)
-
-B = compress_no_matrices(A)
-sm.imsave('compressed_full.jpg', B)
-#sm.imsave('compressed_small.jpg', B1)
-
-A_restored = inverse_transformation(B)
-
-sm.imsave('decompressed.jpg', A_restored)
-
-
-
-
-
 def submatrices(B):
-    M = len(B[:, 0])
-    N = len(B[0, :])
+    B, M, N = preprocess_matrix(B)        
+    
     B1 = B[:M//2,:N//2]
     B2 = B[:M//2,N//2:]
     B3 = B[M//2:,:N//2]
     B4 = B[M//2:,N//2:]
+    
     return B1, B2, B3, B4
 
-B1 = compress(submatrices(B)[0])
-B2 = preprocess_matrix(submatrices(B)[1])[0]
-B3 = preprocess_matrix(submatrices(B)[2])[0]
-B4 = preprocess_matrix(submatrices(B)[3])[0]
+def compress_levels(old, levels):
+    for i in range(levels):
+        old = submatrices(compress(old))[0]
+    
+    return old
 
 
-Q1 = np.column_stack([B1, B2])
-Q2 = np.column_stack([B3, B4])
-A2 = np.vstack([Q1, Q2])
 
-sm.imsave('doublecompressed.jpg',A2)
-sm.imsave('doublecompressed_free.jpg',B1)
+A = sm.imread('kvinna.jpg', True)
+B = compress(A)
+#B_nomat = compress_no_matrices(A)
+#sm.imsave('compressed_nomat.jpg', B_nomat)
+sm.imsave('compressed_full.jpg', B)
+
+A_restored = inverse_transformation(B)
+sm.imsave('decompressed_full.jpg', A_restored)
+
+B1, B2, B3, B4 = submatrices(B)
+
+for n in range(1, 5):
+    sm.imsave('compressed_{}.jpg'.format(n), compress_levels(A, n))
+
+A_restored_piecewise = inverse_transformation_piecewise(B1, B2, B3, B4)
+
+sm.imsave('decompressed_piecewise.jpg', A_restored_piecewise)
