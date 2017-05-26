7613985252b220cc34b222527cf399eec4ad6d28
SimonHalvdansson <simonhalvdansson@gmail.com>
2017-05-26 12:07:19 +0200

    Minor

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import numpy as np
 def create_W(n):
     W = zeros([n,n])
     i = 0
-    scale_factor = 1/sqrt(2) 
+    scale_factor = 1/2#1/sqrt(2) 
     
     for j in range(int(n/2)):
         W[j, i] = scale_factor
@@ -47,9 +47,7 @@ def compress(A):
     
     B = dot(dot(W_M, A), W_N.T)
     
-    #B1, B2, B3, B4 = 0,0,0,0#submatrices(B)
-
-    return B#, B1, B2, B3, B4
+    return B
     
 def compress_no_matrices(A):
     """
@@ -101,6 +99,9 @@ def inverse_transformation(B):
     return A
 
 def submatrices(B):
+    """
+    Takes a full matrix, returns 4 submatrices for each quadrant
+    """
     B, M, N = preprocess_matrix(B)        
     
     B1 = B[:M//2,:N//2]
@@ -111,11 +112,33 @@ def submatrices(B):
     return B1, B2, B3, B4
 
 def compress_levels(old, levels):
+    """
+    Takes an array and compresses it "levels" times, discarding delta
+    """
     for i in range(levels):
         old = submatrices(compress(old))[0]
     
     return old
 
+def lossy_submatrix_compression(A, threshold):
+    A1, A2, A3, A4 = submatrices(A)
+    
+    M = len(A2[:, 0])
+    N = len(A2[0, :])
+    
+    for i in range(M):
+        for j in range(N):
+            if abs(A2[i, j]) <= threshold:
+                A2[i, j] = 0
+            if abs(A3[i, j]) <= threshold:
+                A3[i, j] = 0
+    return A1, A2, A3, A4
+
+
+
+"""
+    Kvinna och tillhörande
+"""
 A = sm.imread('kvinna.jpg', True)
 B = compress(A)
 #B_nomat = compress_no_matrices(A)
@@ -139,21 +162,14 @@ sm.imsave('decompressed_piecewise.jpg', A_restored_piecewise)
 
 
 
-
-
-
-
-
-
+"""
+    Gruppen
+"""
 A = sm.imread('gruppen.jpg', True)
 B = compress(A)
 
-sm.imsave('1.jpg', B)
+
 B1, B2, B3, B4 = submatrices(B)
-#B2 *= 0
-#B3 *= 0
-#B4 *= 0
-#sm.imsave('2.jpg', inverse_transformation_piecewise(B1, B2, B3, B4))
 
 M = len(B2[:, 0])
 N = len(B2[0, :])
@@ -164,19 +180,18 @@ for i in range(M):
         if abs(B2[i, j]) <= 4:
             B2[i, j] = 0
         if abs(B3[i, j]) <= 4:
-            B3[i, j] = 0
-"""
-M = len(B3[:, 0])
-N = len(B3[0, :])
-
-for i in range(M):
-    for j in range(N):
-        #print(B2[i, j])
-"""        
-              
+            B3[i, j] = 0              
 
 sm.imsave('gruppen_lossy.jpg', inverse_transformation_piecewise(B1, B2, B3, B4))
 
 sm.imsave('gruppen_lossless.jpg', inverse_transformation(compress(A)))
+
+
+#downscaled + lossy:
+B = compress_levels(A, 2)
+B_temp = compress(B)
+B1, B2, B3, B4 = lossy_submatrix_compression(B_temp, 1)
+
+sm.imsave('gruppen_downscaled_lossy.jpg', inverse_transformation_piecewise(B1, B2, B3, B4))
 
 
